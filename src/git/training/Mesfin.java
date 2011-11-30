269b824819038ecd086ae303da011f27c623454e
Mesfin Mebrate <mesi91@hotmail.com>
2011-11-30 15:06:54 +0000

    Let's see if this works

--- src/git/training/Mesfin.java
+++ src/git/training/Mesfin.java
@@ -9,5 +9,26 @@ package git.training;
  * @author fayimora
  */
 public class Mesfin {
+    private int age;
+    private String city;
     
+    public Mesfin()
+    {
+        age = 20;
+        city = "London";
+    }
+    
+    public int getAge()
+    {
+        return age;
+    }
+    public String getCity()
+    {
+        return city;
+    }
+    
+    public String tellJoke()
+    {
+        return "Did you fell from heaven, because you look like you've landed on your face.";
+    }
 }
